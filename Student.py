c418aa1deeaa97a69215f54bf63e9b137d0be7f3
Angela Nguyen <angelang@usc.edu>
2022-01-19 16:47:00 -0800

    Complete Student object

---
+++ Student.py
@@ -0,0 +1,9 @@
+"""
+USC Student object for Trojan Check Bot
+"""
+
+class Student:
+    def __init__(self, username, password, phone):
+        self.username = username
+        self.password = password
+        self.phone = phone
